80c558fd562c2b05b8ad639fb853bb06f231035c
afc163 <afc163@gmail.com>
2024-11-24 13:51:06 +0800

    refactor: refactor bubble loading.tsx (#225)

--- components/bubble/loading.tsx
+++ components/bubble/loading.tsx
@@ -4,15 +4,12 @@ interface LoadingProps {
   prefixCls?: string;
 }
 
-const Loading: React.FC<Readonly<LoadingProps>> = (props) => {
-  const { prefixCls } = props;
-  return (
-    <span className={`${prefixCls}-dot`}>
-      <i className={`${prefixCls}-dot-item`} key={`item-${1}`} />
-      <i className={`${prefixCls}-dot-item`} key={`item-${2}`} />
-      <i className={`${prefixCls}-dot-item`} key={`item-${3}`} />
-    </span>
-  );
-};
+const Loading: React.FC<Readonly<LoadingProps>> = ({ prefixCls }) => (
+  <span className={`${prefixCls}-dot`}>
+    <i className={`${prefixCls}-dot-item`} key={`item-${1}`} />
+    <i className={`${prefixCls}-dot-item`} key={`item-${2}`} />
+    <i className={`${prefixCls}-dot-item`} key={`item-${3}`} />
+  </span>
+);
 
 export default Loading;
